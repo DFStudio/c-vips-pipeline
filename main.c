e46f774f6fdba7d07bffb0cbd178cb22ccd5f059
Nick Embrey <nick.embrey@dfstudio.com>
2020-12-13 21:06:55 -0800

    add comments to main.c

--- main.c
+++ main.c
@@ -5,31 +5,51 @@ int main(int argc, char **argv) {
     VipsImage *in;
     VipsImage *out;
 
-    if( argc != 11 ) vips_error_exit( "usage: %s width height image output quality strip autorotate profile sigma x1", argv[0] );
+    /** Note that for the brevity and clarity of the code, all arguments are positional only.
+     * If usage of the utility ever becomes complex enough that flag arguments would improve its quality,
+     * getopt (https://www.gnu.org/software/libc/manual/html_node/Getopt.html) is likely the best option. (Argp is
+     * another standard argument parsing library that has a higher level of abstraction but there's not a Mac port.)
+     */
+    if( argc != 11 ) {
+        vips_error_exit( "usage: %s width height image output quality strip autorotate profile sigma x1", argv[0] );
+    }
 
+    // width and height of the image
     int width = atoi(argv[1]);
     int height = atoi(argv[2]);
+
+    // file paths for input (image) and output
     char *image = argv[3];
     char *output = argv[4];
+
+    // argument for the output quality should be an integer 70-100, even though its type here is a string
     char *quality = argv[5];
+
+    // whether to strip the metadata from the image
     int strip = atoi(argv[6]);
+
+    // whether to autorotate the image based on its EXIF metadata
     int autorotate = atoi(argv[7]);
+
+    // path to the ICC profile
     char *profile = argv[8];
+
+    // sigma is the mask radius to use for unsharp mask
     double sigma = atof(argv[9]);
+
+    // x1 is value for the smoothness threshold at which to apply the unsharp mask
     double x1 = atof(argv[10]);
 
-    char options[200];
-    strcat(options, "[Q=");
-    strcat(options, quality);
-    if( strip != 0) strcat(options, ",strip");
-    strcat(options, ",optimize_coding]");
-
     char output_and_options[200];
-    strcat(output_and_options, output);
-    strcat(output_and_options, options);
+    if( strip == 1) {
+        snprintf(output_and_options, 200, "%s[Q=%s%s", output, quality, ",strip,optimize_coding]");
+    } else {
+        snprintf(output_and_options, 200, "%s[Q=%s%s", output, quality, ",optimize_coding]");
+    }
 
     VIPS_INIT( argv[0] );
 
+    // get a VIPS Image in memory from the input file
     in = vips_image_new_from_file( image, NULL );
 
     if(strcmp("NONE", profile) != 0) {
@@ -37,7 +57,6 @@ int main(int argc, char **argv) {
     } else {
         vips_thumbnail(image, &in, width, "height", height, "no_rotate", !autorotate, "export_profile", profile, NULL);
     }
-
 
     vips_sharpen(in, &out, "sigma", sigma, "x1", x1, NULL );
     g_object_unref( in );
